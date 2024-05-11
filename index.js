1dada99fdde69b3edf7df1c6e713863597c64266
asif883 <asifiqbal95863@gmail.com>
2024-05-11 14:19:58 +0600

    jobs added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app =express();
 const port = process.env.PORT || 5000 ;
 
@@ -7,6 +8,55 @@ const port = process.env.PORT || 5000 ;
 
 app.use(cors());
 app.use(express.json())
+
+
+// career-compass
+// tpXK3k01EiowvdAh
+
+
+console.log(process.env.DB_USER)
+
+const uri = "mongodb+srv://career-compass:tpXK3k01EiowvdAh@cluster0.osztyuf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+
+     const allJobCollection = client.db('JobsDB').collection('job');
+
+    //  add jobs
+    app.get('/jobs' , async(req, res) =>{
+        const cursor = allJobCollection.find();
+        const result = await cursor.toArray();
+        res.send(result)
+    })
+
+    app.post('/jobs' , async(req ,res)=>{
+        const job = req.body;
+        const result = await allJobCollection.insertOne(job);
+        res.send(result)
+    })
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req ,res) =>{
     res.send('server is running')
