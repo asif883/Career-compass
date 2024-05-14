47247a3f3ca3827d37e4fde1da101839c56cf3fe
asif883 <asifiqbal95863@gmail.com>
2024-05-15 00:07:14 +0600

    deploy

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ const port = process.env.PORT || 5000 ;
 app.use(
   cors({
   origin: [
-        'http://localhost:5173'
+        'http://localhost:5173',
+        'https://career-compass-d0501.web.app',
+        'https://career-compass-d0501.firebaseapp.com'
    ],
   credentials: true,
   }),
@@ -53,6 +55,13 @@ const verifyToken =(req, res, next)=>{
   
 }
 
+const cookieOptions = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production" ? true : false,
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+};
+
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -68,18 +77,14 @@ async function run() {
       // console.log(user)
       
       const token = jwt.sign(user , process.env.ACCESS_TOKEN, {expiresIn: '1h'})
-      res.cookie('token' , token, {
-        httpOnly: true,
-        secure: true,
-        sameSite: 'none'
-      })
+      res.cookie('token' , token,cookieOptions)
       .send({success :true})
     });
 
     // logout 
     app.post('/logout' , async( req ,res )=>{
          const user = req.body;
-         res.clearCookie('token' ,{maxAge: 0}).send({success: true})
+         res.clearCookie('token' ,{ ...cookieOptions, maxAge: 0}).send({success: true})
     })
 
 
@@ -175,7 +180,7 @@ async function run() {
       res.send(result)
     })
 
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
